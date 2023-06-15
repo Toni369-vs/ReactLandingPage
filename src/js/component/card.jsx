018ed3c3a0644928c76b68875b8a340ae73bb2a6
Antonio Centenera Mayoral <125910370+Toni369-vs@users.noreply.github.com>
2023-06-15 08:00:23 +0000

    Proyecto finalizado

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -8,7 +8,7 @@ const Card = () => {
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit Esse, autem dolorum modi deleniti placeat dignissimos, eveniet expedita.</p>
-                    <a href="#" className="btn btn-primary">Find Out More!</a>
+                    <a href="#" className="btn btn-primary mt-4">Find Out More!</a>
                 </div>
             </div>
         
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -9,15 +9,15 @@ const Navbar = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div id="menuNavegacion" className="collapse navbar-collapse">
-                    <div className="navbar-nav">
-                        <a className="nav-item nav-link active text-white" href="#">Home</a>
-                        <a className="nav-item nav-link" href="#">About</a>
-                        <a className="nav-item nav-link" href="#">Services</a>
-                        <a className="nav-item nav-link" href="#">Contact</a>
-                    </div>
-                </div>
             </nav>
+            <div id="menuNavegacion" className="d-flex justify-content end">
+                <div className="navbar navbar-expand-md">
+                    <a className="nav-item nav-link active text-white m-2" href="#">Home</a>
+                    <a className="nav-item nav-link m-2" href="#">About</a>
+                    <a className="nav-item nav-link m-2" href="#">Services</a>
+                    <a className="nav-item nav-link m-2" href="#">Contact</a>
+                </div>
+            </div>
         </div>
     );
 };
